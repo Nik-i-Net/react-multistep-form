9e9b190404b6e398ea37eeb637f9157ad40175fd
Nik-i-Net <netesnik02@gmail.com>
2026-07-08 13:36:04 +0000

    feat: add `StepNav`

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import StepNav from "./StepNav";
+import { useReducer } from "react";
+import { FormContext, formReducer, initialState } from "./state/FormContext";
 import PersonalInfoStep from "./steps/01-PersonalInfo";
+import StepNav from "./StepNav";
 
 function App() {
+  const [state, dispatch] = useReducer(formReducer, initialState);
+
   return (
     <div>
-      <StepNav></StepNav>
-      <PersonalInfoStep></PersonalInfoStep>
+      <FormContext value={{ state, dispatch }}>
+        <StepNav />
+        <PersonalInfoStep />
+      </FormContext>
     </div>
   );
 }
--- src/StepNav.tsx
+++ src/StepNav.tsx
@@ -1,5 +1,40 @@
+import { useFormContext } from "./state/FormContext";
+
+const STEPS = [
+  { number: 1, label: "Your info" },
+  { number: 2, label: "Select plan" },
+  { number: 3, label: "Add-ons" },
+  { number: 4, label: "Summary" },
+];
+
 function StepNav() {
-  return <nav>StepNav</nav>;
+  const { state } = useFormContext();
+
+  return (
+    <nav
+      className="px-6 py-8 text-white w-68.5 h-142 flex flex-col gap-6 bg-no-repeat bg-cover rounded-xl
+        bg-[url('/bg-sidebar-desktop.svg')] max-md:bg-[url('/bg-sidebar-mobile.svg')]"
+    >
+      {STEPS.map((step) => (
+        <div key={step.number} className="flex items-center gap-4">
+          <button
+            className={`rounded-full w-8 h-8 flex items-center justify-center font-medium
+            ${
+              step.number === state.activeStep
+                ? "bg-selected text-primary"
+                : "border border-white text-white"
+            }`}
+          >
+            {step.number}
+          </button>
+          <div className="hidden md:block">
+            <p className="text-muted text-sm uppercase">Step {step.number}</p>
+            <p className="text-white font-medium uppercase">{step.label}</p>
+          </div>
+        </div>
+      ))}
+    </nav>
+  );
 }
 
 export default StepNav;
